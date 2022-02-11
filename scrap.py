24848b9b2d3924529ad334be9f7c7d68627508bd
Sergio De Luca <sdlbsso@gmail.com>
2022-02-11 01:10:52 -0300

    hacer que guarde la session

--- scrap.py
+++ scrap.py
@@ -1,15 +1,17 @@
 import time
 import requests
 
+
 param1 = {'zona':'0080', 'codigo':'3', 'tipo':'1'}
 param_buscabase = {'site' : '18'}
 param_armaelec = {'site' : '18'}
 param_armasecc = {'site' : '18', 'zona' : 't_secc'}
 param_buscaelecc = {'site' : '18', 'orden' : '0'}
 
-#response = requests.get('https://www.padron.gob.ar/publica/armasite.php', data = param_buscabase)
 r = requests.get('https://www.padron.gob.ar/publica/armasite.php')
-print(r.text)
+
 time.sleep(5)
+
 r = requests.post('https://www.padron.gob.ar/publica/mesasres.php', data = param1)
-print(r.text)
+
+

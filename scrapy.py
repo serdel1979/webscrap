60e2db9afb5f775465f00e1f01c3f958be6adfdb
Sergio De Luca <sdlbsso@gmail.com>
2022-02-16 02:29:02 -0300

    estructuras de control

--- scrapy.py
+++ scrapy.py
@@ -9,57 +9,63 @@ from bs4 import BeautifulSoup
 import time
 import pandas as pd
 import csv
-import sys
-
-
-#procesamiento de parámetros
-
-#print("Número de parámetros: ", len(sys.argv))
-#print("Lista de argumentos: ", sys.argv)
-#print(type(sys.argv[1]))
-#print(type(sys.argv[2]))
-#print(type(sys.argv[3]))
+# Armo la lista de secciones
+secciones = []
+for i in range(137):
+    val = ("0000"+str(i+1))[-5:]
+    secciones.append(val)
+secciones.append("00998")
+secciones.append("00999")
 
 
 options = webdriver.ChromeOptions()
 options.add_argument('--disable-extensions')
-#options.headless = True
+options.headless = True
 
 driver = webdriver.Chrome(chrome_options=options)
 
-driver.get('https://www.padron.gob.ar/publica/')
+driver.get('https://www.padron.gob.ar/publica/') # <-- poner en el catch
 time.sleep(1)
 
 select = Select(driver.find_element_by_id('site'))
 select.select_by_visible_text('BUENOS AIRES')
 
 time.sleep(1)
-# elecciones = True
-# while elecciones
-select = Select(driver.find_element_by_id('elec'))
-select.select_by_value('1')
-
-time.sleep(1)
-
-try:
+for i in secciones:
     select = Select(driver.find_element_by_id('secm'))
-    select.select_by_value("00001")
-except:
-    print("No se requiere sección")
+    select.select_by_value(i)
+    print("------------->", select)
+    
+    time.sleep(1)
+
+    hayMesa = True
+    numMesa = 1
+
+    while hayMesa:
+        input = driver.find_element_by_id('mesa')
+        input.clear()
+        input.send_keys(str(numMesa))
+
+        time.sleep(1)
+        btn = driver.find_element_by_id('btnVer')
+        btn.click()
+
+        time.sleep(1)
+        try:
+            btn = driver.find_element_by_id('btnVolver')
+            btn.click()
+            numMesa = numMesa + 1
+            time.sleep(1)
+        except:
+            #Si falla es porque se terminaron las mesas
+            hayMesa = False
+            driver.get('https://www.padron.gob.ar/publica/') 
+            time.sleep(1)
+            select = Select(driver.find_element_by_id('site'))
+            select.select_by_visible_text('BUENOS AIRES')
+            time.sleep(1)
 
 
-
-time.sleep(1)
-
-input = driver.find_element_by_id('mesa')
-input.send_keys('1')
-
-time.sleep(1)
-
-btn = driver.find_element_by_id('btnVer')
-btn.click()
-
-time.sleep(1)
 
 # Hasta acá va el while de mesas
 
